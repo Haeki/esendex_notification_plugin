a792c043aeafca63ab3de330e050fd183745988d
Mika Busch <mika.busch@nagarro.com>
2023-04-20 10:06:56 +0200

    Removed deprecated allow_empty field

--- web/plugins/wato/esendex.py
+++ web/plugins/wato/esendex.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
-# (c) 2020 Nagarro Allgeier ES GmbH
-#          Mika Busch <mika.busch@nagarro-es.com>
+# (c) 2022 Nagarro ES GmbH
+#          Mika Busch <mika.busch@nagarro.com>
 
 # This is free software;  you can redistribute it and/or modify it
 # under the  terms of the  GNU General Public License  as published by
@@ -15,6 +15,8 @@
 # to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
 # Boston, MA 02110-1301 USA.
 
+# Version 2.3 (2023-04-19)
+
 register_notification_parameters("esendex", Dictionary(
     optional_keys=['sms_header', 'max_lenght', 'sub_pattern'],
     elements=[
@@ -22,7 +24,6 @@ register_notification_parameters("esendex", Dictionary(
             title=_("User Name"),
             help=_("Esendex username"),
             size=40,
-            allow_empty=False,
         )),
         ("api_password", TextAscii(
             title=_("API Password"),
@@ -30,32 +31,27 @@ register_notification_parameters("esendex", Dictionary(
                    "If you don't have an API password yet, you can log in and generate one on your "
                    "<a href=\"https://www.esendex.com/profile\">user profile</a>"),
             size=40,
-            allow_empty=False,
         )),
         ("account_reference", TextAscii(
             title=_("Account Reference"),
             help=_("You need to provide your essendex account reference"),
             size=40,
-            allow_empty=False,
         )),
         ("sms_header", TextAscii(
             title=_("SMS Header"),
             help=_("Header added to the start of each SMS"),
             size=40,
-            allow_empty=False,
         )),
         ("sub_pattern", TextAscii(
             title=_("Service output sub pattern"),
             help=_("(Regexpattern) Matches get removed from the service output"),
             size=40,
-            allow_empty=False,
             default_value="WARN - |CRIT - |OK - ",
         )),
         ("max_lenght", Integer(
             title=_("Max Message Length"),
             help=_("Maximum number of characters bevor message gets cut off"),
             size=40,
-            allow_empty=False,
             default_value=160,
         )),
     ]
